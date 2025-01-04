fbfd88094a1af8c1b7f5ad7534013ef4e235bb14
fjcorfel <fjcorfel@gmail.com>
2025-01-04 17:59:37 +0100

    Refactor PCA.py for improved clarity in comments

--- PCA.py
+++ PCA.py
@@ -8,8 +8,7 @@
 # conjunto de datos de la flor Iris. Por consola se muestra el porcentaje de
 # varianza explicada por cada componente principal. Además, se generan dos
 # gráficas: una en 2D y otra en 3D, mostrando la representación gráfica de las
-# las dos primeras componentes principales y las tres primeras componentes
-# principales, respectivamente.
+# las dos y tres primeras componentes principales, respectivamente.
 #
 # Autor: Francisco Javier Cordero Felipe
 # Fecha: 20/12/2024
@@ -21,50 +20,52 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn import datasets as ds
 
-# Load the iris dataset
+# Cargar el conjunto de datos de la flor Iris
 data = ds.load_iris(as_frame=True).frame
 
-# Define the flower species
+# Definir los nombres de las especies
 species_names = ["I. setosa", "I. versicolor", "I. virginica"]
 
-# Select the features
-# In X we ignore the target variable (flower species)
+# Selección de las variables 
+# En X se ignora la variable "target" (especie de la flor)
 X = data.iloc[:, 0:-1].values
-# In y we store the target variable
+# En y se almacena la variable "target"
 y = data.iloc[:, -1].values
 
-# Standardize the data (mean=0, standard deviation=1)
+# Estandarizar los datos (media=0, desviación estándar=1)
 X_mean = np.mean(X, axis=0)
 X_std = np.std(X, axis=0)
 X_standardized = (X - X_mean) / X_std
 
-# Build the covariance matrix
-# rowvar=False -> the columns are the variables
+# Construir la matriz de covarianza
+# rowvar=False -> las columnas representan las variables
 S = np.cov(X_standardized, rowvar=False)
 
-# Obtain the eigenvectors and eigenvalues
+# Obtener vectores y valores propios de S
 eigenvalues, eigenvectors = np.linalg.eig(S)
 
-# Sort the eigenvectors by decreasing eigenvalues 
+# Ordenar los vectores propios por valores propios decrecientes
 sorted_idxs = np.argsort(eigenvalues)[::-1]
 eigenvalues = eigenvalues[sorted_idxs]
 eigenvectors = eigenvectors[:, sorted_idxs]
 
 # Project the data onto the new space
+# Proyectar los vectores de características en el nuevo espacio
 Y = X_standardized @ eigenvectors
 
-# Calculate the explained variance
+# Calcular la varianza explicada por cada componente principal
 explained_variance = eigenvalues / np.sum(eigenvalues)
 
-# Print the explained variance
+# Mostrar la varianza explicada por cada componente principal
 print(f"Explained variance by each principal component:")
 for i, ratio in enumerate(explained_variance):
     print(f"* PC{i + 1}: {ratio * 100:.2f}%")
 
 # Plot the PCA
+# Representación gŕafica de la PCA
 fig = plt.figure(figsize=(12, 6))
 
-# 2D plot
+# Plot 2D
 ax1 = fig.add_subplot(121)
 for label, species in zip(np.unique(y), species_names):
     ax1.scatter(Y[y == label, 0],
@@ -77,7 +78,7 @@ ax1.set_title("2D PCA - Iris Dataset")
 ax1.legend()
 ax1.grid()
 
-# 3D plot
+# Plot 3D
 ax2 = fig.add_subplot(122, projection='3d')
 for label, species in zip(np.unique(y), species_names):
     ax2.scatter(Y[y == label, 0],
